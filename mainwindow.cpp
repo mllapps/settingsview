a544f8755be424aa84f039f1e4491f666258ab49
fl0mll <mueller@mllapps.com>
2016-06-07 10:02:18 +0200

    create sidebar with buttons with fixed position on top

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,23 +5,29 @@
 
 #include <QTextBrowser>
 #include <QLabel>
+#include <QPushButton>
+#include <QSpacerItem>
 
 MainWindow::MainWindow(QWidget *parent) :
     QWidget(parent)
 {
+    QVBoxLayout * sidebarLayout = new QVBoxLayout();
+    sidebarLayout->addWidget(new QPushButton("General"));
+    sidebarLayout->addWidget(new QPushButton("Network"));
+    sidebarLayout->addWidget(new QPushButton("Slideshow"));
+    sidebarLayout->addWidget(new QPushButton("Privacy"));
+    sidebarLayout->addWidget(new QPushButton("Advanced"));
+    sidebarLayout->addSpacerItem(new QSpacerItem(0,0,QSizePolicy::Minimum, QSizePolicy::MinimumExpanding));
+
+    QWidget * sidebar = new QWidget();
+    sidebar->setLayout(sidebarLayout);
+
     QTextBrowser *centralWidget = new QTextBrowser;
     centralWidget->setPlainText(tr("Central widget"));
 
     BorderLayout *layout = new BorderLayout;
     layout->addWidget(centralWidget, BorderLayout::Center);
-    layout->addWidget(createLabel("North"), BorderLayout::North);
-    layout->addWidget(createLabel("West 1"), BorderLayout::West);
-    layout->addWidget(createLabel("West 200000000"), BorderLayout::West);
-    layout->addWidget(createLabel("West 3"), BorderLayout::West);
-    layout->addWidget(createLabel("East 1"), BorderLayout::East);
-    layout->addWidget(createLabel("East 2") , BorderLayout::East);
-    layout->addWidget(createLabel("East 3") , BorderLayout::East);
-    layout->addWidget(createLabel("South"), BorderLayout::South);
+    layout->addWidget(sidebar, BorderLayout::West);
     setLayout(layout);
 
     setWindowTitle(tr("Border Layout"));
